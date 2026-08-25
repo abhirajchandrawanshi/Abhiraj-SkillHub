e0f5052e2ba5c102c5e5fbaa9f8e8c58010582c0
Abhiraj Chandrawanshi <abhirajchandrawanshi18@gmail.com>
2026-08-25 18:10:45 +0530

    configure Nitro for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -10,6 +11,7 @@ export default defineConfig({
         entry: "server",
       },
     }),
+    nitro(),
     react(),
     tailwindcss(),
   ],
