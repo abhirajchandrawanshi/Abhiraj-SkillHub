70e7b861dabc9bb6499099363137da4e02d45ae2
Abhiraj Chandrawanshi <abhirajchandrawanshi18@gmail.com>
2026-08-25 17:54:08 +0530

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,15 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [tanstackStart({ server: { entry: "server" } }), react(), tailwindcss()],
+  plugins: [
+    tanstackStart({
+      server: {
+        entry: "server",
+      },
+    }),
+    react(),
+    tailwindcss(),
+  ],
   resolve: {
     tsconfigPaths: true,
   },
